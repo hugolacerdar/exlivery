89e2792e91d50cb89185eb63262add2c2ca4286e
Hugo <hugolacerdar@gmail.com>
2021-05-10 14:15:50 -0300

    Adding Exmachina lib

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule Exlivery.MixProject do
       app: :exlivery,
       version: "0.1.0",
       elixir: "~> 1.11",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,8 +22,12 @@ defmodule Exlivery.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_machina, "~> 2.7.0"},
       {:decimal, "~> 2.0"},
       {:credo, "~> 1.5", only: [:test, :dev], reuntime: false}
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 end
