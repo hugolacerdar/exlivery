dd6d420512e64aed4e3618c7696f870d516158f1
Hugo <hugolacerdar@gmail.com>
2021-05-11 20:24:20 -0300

    Adding item and order structs to factory

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,6 +1,7 @@
 defmodule Exlivery.Factory do
   use ExMachina
 
+  alias Exlivery.Orders.{Item, Order}
   alias Exlivery.Users.User
 
   def user_factory do
@@ -10,6 +11,32 @@ defmodule Exlivery.Factory do
       cpf: "12312313222",
       age: 26,
       address: "Rua das Bananas"
+    }
+  end
+
+  def item_factory do
+    %Item{
+      category: :pizza,
+      description: "Vegfredo",
+      quantity: 1,
+      unity_price: Decimal.new(35)
+    }
+  end
+
+  def order_factory do
+    %Order{
+      delivery_address: "Rua das Bananas",
+      items: [
+        build(:item),
+        build(:item,
+          description: "Hamburguer de falafel",
+          category: :hamburguer,
+          quantity: 2,
+          unity_price: Decimal.new("18.50")
+        )
+      ],
+      total_price: Decimal.new("72.00"),
+      user_cpf: "12312313222"
     }
   end
 end
